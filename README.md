8fa84e29b8260d66d128e346ac5e8f48cbdd8134
Benoit Nadeau <benad@me.com>
2025-08-27 21:54:00 -0400

    Initial release

---
+++ README.md
@@ -0,0 +1,105 @@
+# gh-fetch-release
+
+## Usage
+
+```
+python3 gh-fetch-release.py [-h] --repo REPO --pattern PATTERN --outdir OUTDIR --binfiles BINFILES
+                            [--downloaddir DOWNLOADDIR] [--setexec] [--rename RENAME]
+
+Fetch and extract binary files from the latest GitHub release of the given repository.
+
+options:
+  -h, --help            show this help message and exit
+  --repo REPO           GitHub repository in the form owner/repo
+  --pattern PATTERN     Regex pattern to match the asset filename
+  --outdir OUTDIR       Output directory to install the binary files.
+  --binfiles BINFILES   Glob pattern to match the binary files to install from the extracted files
+  --downloaddir DOWNLOADDIR
+                        Temporary download directory (if not given, a temporary directory will be created and deleted)
+  --setexec             Set executable permission on the installed binary files
+  --rename RENAME       Set the name of the installed binary file (only if a single file is matched by --binfiles)
+```
+
+Requirements:
+
+- Python 3
+- `curl`
+- `tar`
+- `unzip` (optional)
+- `unzstd` (from the `zstd` package, optional)
+
+Examples:
+
+```
+python gh-fetch-release.py \
+  --repo microsoft/edit \
+  --pattern 'x86_64-linux-gnu\.tar\.zst$' \
+  --outdir ~/bin \
+  --binfiles 'edit' \
+  --setexec
+```
+
+```
+python gh-fetch-release.py \
+  --repo jarun/nnn \
+  --pattern 'musl-static-.*\.x86_64\.tar\.gz$' \
+  --outdir ~/bin \
+  --binfiles 'nnn-musl-static' \
+  --setexec \
+  --rename 'nnn'
+```
+
+```
+python gh-fetch-release.py \
+  --repo rclone/rclone \
+  --pattern 'linux-amd64\.zip$' \
+  --outdir ~/bin \
+  --binfiles 'rclone-*/rclone' \
+  --setexec
+```
+
+```
+python gh-fetch-release.py \
+  --repo dandavison/delta \
+  --pattern 'x86_64-unknown-linux-musl\.tar\.gz$' \
+  --outdir ~/bin \
+  --binfiles 'delta-*/delta' \
+  --setexec
+```
+
+## Background
+
+I have a bunch of Linux tools that I install in `~/bin` because either
+they are not available's in the system's package manager, or if it's
+there it is quite outdated. Those tools' binaries are often from GitHub.
+
+My initial version of this tool looked like this:
+
+```
+curl -s https://api.github.com/repos/dandavison/delta/releases/latest | \
+jq -r '.assets[] | select(.name | test("delta-.*-x86_64-unknown-linux-musl\\.tar\\.gz$")) | .browser_download_url' | \
+xargs -I {} curl -sL {} -o - | \
+tar -C /tmp -xzvf - && \
+cp /tmp/delta-*-unknown-linux-musl/delta ~/bin && \
+rm -rf /tmp/delta-*-unknown-linux-musl
+```
+
+Sure, it's a clever one-liner, but then it requires installing [`jq`](https://jqlang.org/),
+and tweaking it for each different tool is a mess.
+
+I looked at a few alternatives, like [binup](https://github.com/KonishchevDmitry/binup),
+[eget](https://github.com/zyedidia/eget), [hubapp](https://github.com/warrensbox/hubapp) and
+[getghrel](https://github.com/kavishgr/getghrel) (to name a few), and they were not to my
+liking for the following reasons:
+
+- Limited control on how to identify the binary from the extracted package, and where to
+  place them, possibly after renaming them and adding the executable bit.
+- They are all binaries themselves, which doesn't help if they don't support your CPU
+  architecture (for example arm64).
+- Some require interactive use or maintaining configuration files.
+
+So I went with a simpler approach. I assume the Linux system has at least
+Python 3, `curl`, `tar` and maybe `unzip` and `unzstd`. This seems reasonable.
+And then, copy over a single-file Python script with no additional dependencies
+and run it. The script is samll enough that it could be copied over an SSH shell
+(`cat > gh-fetch-release.py`, paste, control-d).
---
+++ gh-fetch-release.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+
+# MIT License
+# Copyright (c) 2025 Benoit Nadeau
+# See LICENSE file for details.
+
+# Fetch and extract binary files from the latest GitHub
+# release of the given repository.
+
+import argparse
+import glob
+import json
+import os
+import os.path
+import re
+import shutil
+import tempfile
+import subprocess
+
+def get_cli_options() -> dict:
+    parser = argparse.ArgumentParser(
+                            prog='gh-fetch-release',
+                            description='''Fetch and extract binary files from the latest GitHub
+                            release of the given repository.''',
+                            add_help=True,
+                            allow_abbrev=True,
+                            exit_on_error=True)
+    parser.add_argument('--repo', type=str, required=True,
+                        help='GitHub repository in the form owner/repo')
+    parser.add_argument('--pattern', type=str, required=True,
+                        help='Regex pattern to match the asset filename')
+    parser.add_argument('--outdir', type=str, required=True,
+                        help='Output directory to install the binary files.')
+    parser.add_argument('--binfiles', type=str, required=True,
+                        help='Glob pattern to match the binary files to install from the extracted files')
+    parser.add_argument('--downloaddir', type=str, required=False, default=None,
+                        help='''Temporary download directory (if not given, a temporary
+                                directory will be created and deleted)''')
+    parser.add_argument('--setexec', action='store_true', required=False, default=False,
+                        help='Set executable permission on the installed binary files')
+    parser.add_argument('--rename', type=str, required=False, default=None,
+                        help='''Set the name of the installed binary file (only if a single file is
+                                matched by --binfiles)''')
+    args = parser.parse_args()
+    return vars(args)
+
+def get_download_url(options) -> str | None:
+    owner, repo = options['repo'].split('/')
+
+    api_url = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
+    try:
+        response_data = subprocess.check_output(
+            ["curl", "-sL", "-H", "User-Agent: python", api_url],
+            text=True
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Error fetching release info: {e}")
+        return None
+    response = json.loads(response_data)
+    if 'assets' not in response:
+        raise Exception("No assets found in the latest release.")
+    for asset in response['assets']:
+        if not ( 'name' in asset and 'browser_download_url' in asset ):
+            continue
+        print(f"Checking asset: {asset['name']} against pattern {options['pattern']}")
+        if re.search(options['pattern'], asset['name']) is not None:
+            return asset['browser_download_url']
+
+def download_file(url: str, path: str) -> int:
+    try:
+        res = subprocess.call(["curl", "-L", "-o", path, url])
+    except Exception as e:
+        print(f"Error downloading file: {e}")
+        res = 1
+    return res
+
+def extract_binfiles(download_filename: str, download_path: str, downloaddir: str) -> int:
+    class Archive:
+        def __init__(self, path: str):
+            self.path = path
+
+        def extract(self, outdir: str) -> int:
+            return 1 # Not implemented
+
+    class ArchiveTarGz(Archive):
+        def extract(self, outdir: str) -> int:
+            return os.system(f"tar -xzf {self.path} -C {outdir}")
+
+    class ArchiveZip(Archive):
+        def extract(self, outdir: str) -> int:
+            return os.system(f"unzip -o {self.path} -d {outdir}")
+
+    class ArchiveTarZst(Archive):
+        def extract(self, outdir: str) -> int:
+            return os.system(f"tar --use-compress-program=unzstd -xf {self.path} -C {outdir}")
+
+    archive_classes = {
+        '.tar.gz': ArchiveTarGz,
+        '.zip': ArchiveZip,
+        '.tar.zst': ArchiveTarZst
+    }
+
+    archive_obj = None
+    for ext, cls in archive_classes.items():
+        if download_filename.endswith(ext):
+            archive_obj = cls(download_path)
+            break
+
+    if archive_obj is None:
+        raise Exception(f"Unsupported archive format: {download_filename}")
+
+    return archive_obj.extract(downloaddir)
+
+def run(options: dict) -> None:
+    url = get_download_url(options)
+    if url is None:
+        raise Exception("No matching asset found in the latest release.")
+
+    downloaddir = options.get("downloaddir", None)
+    temporary_dir = None
+    if downloaddir is None:
+        temporary_dir = tempfile.TemporaryDirectory(delete=False)
+        downloaddir = temporary_dir.name
+    else:
+        os.makedirs(downloaddir, exist_ok=True)
+
+    print(f"Downloading {url} to {downloaddir}")
+
+    try:
+        download_filename = os.path.basename(url)
+        download_path = os.path.join(downloaddir, download_filename)
+        res = download_file(url, download_path)
+
+        if res != 0:
+            raise Exception(f"Failed to download {url}")
+
+        print("Extracting to", downloaddir)
+        res = extract_binfiles(download_filename, download_path, downloaddir)
+        if res != 0:
+            raise Exception(f"Failed to extract {download_path} to {downloaddir}")
+
+        matching_files = glob.glob(os.path.join(downloaddir, options['binfiles']))
+        if len(matching_files) == 0:
+            raise Exception(f"Binary file {options['binfiles']} not found in the extracted files.")
+
+        outdir = options["outdir"]
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+
+        for file in matching_files:
+            outfilepath = os.path.join(outdir, os.path.basename(file))
+            if len(matching_files) == 1 and options.get("rename", None) is not None:
+                outfilepath = os.path.join(outdir, options["rename"])
+            shutil.copyfile(file, outfilepath)
+            if options.get("setexec", False):
+                os.chmod(outfilepath, 0o755)
+            print(f"Installed {file} as {outfilepath}")
+    finally:
+        if temporary_dir is not None:
+            temporary_dir.cleanup()
+
+if __name__ == "__main__":
+    options = get_cli_options()
+    run(options)
